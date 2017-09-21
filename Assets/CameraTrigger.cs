85c7aed45308f04348b2898c652d4368b20aee05
Unknown <kelseyhu2425@gmail.com>
2017-09-21 10:50:02 -0400

    First commit

---
+++ Assets/CameraTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour {
+
+	public Transform lookOverride; // set new lookat value
+	public Transform moveOverride; // set new move value
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider activator)
+	{
+		CameraControll camControl = Camera.main.GetComponent<CameraControll>();
+
+		//override the values in CamControll
+		if (moveOverride != null)
+		{
+			camControl.moveToTarget = moveOverride;
+		}
+		if (lookOverride != null)
+		{
+			camControl.lookAtTarget = lookOverride;
+		}
+	}
+
+	//draw lines in the scene view
+	void OnDrawGizmos()
+	{
+		if (moveOverride != null)
+		{
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawLine(transform.position, moveOverride.position);}
+
+		if (lookOverride != null)
+		{
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawLine(transform.position, lookOverride.position);}
+	}
+}
